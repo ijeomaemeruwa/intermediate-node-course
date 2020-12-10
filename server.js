67b7a63f62d4ff1cebf2c8c568d98166cc86d265
ijeomaemeruwa <helloijeoma@gmail.com>
2020-12-10 01:34:01 +0000

    made a delete request

--- server.js
+++ server.js
@@ -73,6 +73,26 @@ app.route('/users/:id')
   )
 })
 // DELETE
-.delete((req,res)=>{
-  // User.findByIdAndDelete()
-})
+  .delete((req,res)=>{
+    User.findByIdAndDelete(
+      req.params.id,
+      (err,data)=>{
+        if (err){
+          res.json({
+            success: false,
+            message: err
+          })
+        } else if (!data){
+          res.json({
+            success: false,
+            message: "Not Found"
+          })
+        } else {
+          res.json({
+            success: true,
+            data: data
+          })
+        }
+      }
+    )
+  })
